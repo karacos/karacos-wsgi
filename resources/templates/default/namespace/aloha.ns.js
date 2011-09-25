657ba8d8116764402afb4fae26a55ab668852c0b
nka11 <nka11@nka.me>
2011-09-25 22:48:07 +0200

    requirejs demendency mechanism

--- resources/templates/default/namespace/aloha.ns.js
+++ resources/templates/default/namespace/aloha.ns.js
@@ -1,18 +1,17 @@
 <%def name="activate_editor(instance)">
-	<%include file="${instance.get_aloha_template_uri()}"/>
-	KaraCos('body').bind('aloha',function(){
-		KaraCos('[about="urn:uuid:${instance.id}"]').each(function(){
-			var semantic_entity = this;
-//			Aloha.bind('alohaI18nPluginsLoaded',
-//				function(){
-				KaraCos.$(semantic_entity).vieSemanticAloha();
-//				});
+	##<%include file="${instance.get_aloha_template_uri()}"/>
+	require(['order!karacos/conf/aloha-default','order!aloha'], function() {
+		$('body').bind('aloha',function(){
+			console.log("activating editor for semantic entity");
+			$('[typeof][about]').find("[property]").aloha();
 		});
 	});
 </%def>
 <%def name="save_instance(instance)">
-	var objects = KaraCos('[about="urn:uuid:${instance.id}"]'), model;
+(function($,undefined) {
+	var objects = $('[typeof][about="urn:uuid:${instance.id}"]'), model;
 	model = VIE.ContainerManager.getInstanceForContainer(objects);
 	//Backbone.sync('_update',model);
 	model.save();
+})(window.alohaQuery);
 </%def>
